5d0e6c09cd180a72fb8a4bd2b3d907d0ad75f227
Jacked-sec <same16912@gmail.com>
2025-02-20 15:37:31 +0700

    Update Devo provisioning API2 V2

--- Devo provisioning API2 V2.py
+++ Devo provisioning API2 V2.py
@@ -3,15 +3,15 @@ import hmac
 import hashlib
 import json
 
-api_key = 'xxx'
-api_secret = 'xxx'
+api_key = 'xxxxx'
+api_secret = 'xxxxx'
 timestamp = str(int(time.time()) * 1000)
 
 # Convert the data to a JSON string (Creates a new domain)
 data1 = json.dumps({"name": "xxxx@xxxx", "plan": "gable_children"})
 
 # Adds an internal user
-data2 = json.dumps({"domain": "xxxx@xxxx","userName": "xxxx xxxx","email": "xxxx.x@xxxx.com","role": "Owner"})
+data2 = json.dumps({"domain": "xxxx@xxxx","userName": "xxxx xxxx","email": "xxxx@xxxx.com","role": "Owner"})
 
 # Create the signature(Creates a new domain)
 sign1 = hmac.new(bytes(api_secret, 'utf-8'), bytes(api_key + data1 + timestamp, 'utf-8'), hashlib.sha256)
@@ -24,12 +24,12 @@ sign2 = sign2.hexdigest()
 
 
 print("Creates a new domain")
-print(sign1)
-print(timestamp)
+print("x-logtrust-sign =",sign1)
+print("x-logtrust-timestamp =",timestamp)
 print(data1)
 print("Creates a new user")
-print(sign2)
-print(timestamp)
+print("x-logtrust-sign =",sign2)
+print("x-logtrust-timestamp =",timestamp)
 print(data2)
 
 #Use in - > https://api-apac.devo.com/probio/apiDoc/index.html#/reseller/post_domain
